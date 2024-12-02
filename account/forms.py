1d1e2949634049712b35e3cd11e6b96abbe1843f
student08 <student@gmail.com>
2024-12-02 14:40:29 +0100

    pliki bookmarks do sciongania

--- account/forms.py
+++ account/forms.py
@@ -1,28 +1,36 @@
 from django import forms
 from django.contrib.auth import get_user_model
-from django.forms import ModelForm
 
 from account.models import Profile
 
 
 class LoginForm(forms.Form):
-    username=forms.CharField()
+    username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
 
+
 class UserRegistrationForm(forms.ModelForm):
-    password=forms.CharField(widget=forms.PasswordInput, label='Haslo')
-    password1 = forms.CharField(widget=forms.PasswordInput, label='powtoz haslo')
+
+    password = forms.CharField(widget=forms.PasswordInput, label='Hasło')
+    password2 = forms.CharField(widget=forms.PasswordInput, label='Powtórz hasło')
+
     class Meta:
-        model=get_user_model()
-        fields=['username','first_name','email']
-    def clean_passsword2(self):
-        if self.cleaned_data['password'] != self.cleaned_data['password1']:
-            raise forms.ValidationError('hasla nie takie same')
+        model = get_user_model()
+        fields = ['username', 'first_name', 'email']
+
+    def clean_password2(self):
+        if self.cleaned_data['password'] != self.cleaned_data['password2']:
+            raise forms.ValidationError("Hasła są różne!")
+
+        return self.cleaned_data['password2']
+
+
 class UserEditForm(forms.ModelForm):
     class Meta:
-        model=get_user_model()
-        fields=['first_name','last_name','email']
+        model = get_user_model()
+        fields = ['first_name', 'last_name', 'email']
+
 class ProfileEditForm(forms.ModelForm):
     class Meta:
-        model=Profile
-        fields=['date_of_birth','photo']
+        model = Profile
+        fields = ['date_of_birth', 'photo']
--- account/views.py
+++ account/views.py
@@ -1,54 +1,76 @@
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-
-from django.contrib.auth import authenticate, login
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from account.forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from account.models import Profile
 
 
+# Create your views here.
 def user_login(request):
-    if request.method=='POST':
+    if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
-            cd=form.cleaned_data
-            user=authenticate(username=cd['username'],password=cd['password'])
+            cd = form.cleaned_data
+            user = authenticate(username=cd['username'],
+                                password=cd['password'])
             if user is not None:
                 if user.is_active:
                     login(request, user)
-                    return HttpResponse('succes')
+                    return HttpResponse('Uwierzytelnienie zakończyło się sukcesem')
                 else:
-                    return HttpResponse('konta nie ma')
+                    return HttpResponse('Konto jest zablokowane')
             else:
-                return HttpResponse('nie udalo sie uwierzytelnic')
+                return HttpResponse('Nieprawidłowe dane uwierzytelniające')
+
     else:
-        form=LoginForm()
-    return render(request, 'account/login.html', {'form':form})
+        form = LoginForm()
+    return render(request, 'account/login.html', {'form': form})
+
 
 @login_required
 def dashboard(request):
-    return render(request, 'account/dashboard.html',{'section': 'dashboard'})
+    return render(request, 'account/dashboard.html',
+                  {'section': 'dashboard'})
+
 
 def register(request):
-    if request.method=='POST':
-        user_form=UserRegistrationForm(request.POST)
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
         if user_form.is_valid():
-            new_user=user_form.save(commit=False)
+            new_user = user_form.save(commit=False)
             new_user.set_password(user_form.cleaned_data['password'])
             new_user.save()
             Profile.objects.create(user=new_user)
-            return render(request,'account/registration_done.html', {'user_form': user_form})
+            return render(request, 'account/register_done.html', {'new_user': new_user})
     else:
-        user_form=UserRegistrationForm
-    return render(request, 'account/registration.html', {'user_form': user_form})
+        user_form = UserRegistrationForm()
+
+    return render(request, 'account/register.html', {'user_form': user_form})
+
 
 @login_required
 def edit(request):
-    if request.method=='POST':
-        pass
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+        profile_form = ProfileEditForm(instance=request.user.profile,
+                                       data=request.POST,
+                                       files=request.FILES)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Profil został zaktualizowany')
+            return redirect('dashboard')
+        else:
+            messages.error(request,"Wystąpił błąd w formularzu")
     else:
-        user_form=UserEditForm(instance=request.user)
-        profile_form=ProfileEditForm(instance=request.user.profile)
-    return render(request,'account/edit.html',
-                  {'user_form':user_form,'profile_form':profile_form})
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+    return render(request, 'account/edit.html', {
+        'user_form': user_form,
+        'profile_form': profile_form
+    })
+
